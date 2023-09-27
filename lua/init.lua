2ee2b58243f03df76b1b8474f2a12ff333e68af2
Chris Nguyen <chrisnguyen@Chriss-Mac-mini.local>
2023-09-27 11:31:30 +0700

    feat: checking setup

--- lua/init.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-vim.api.nvim_create_user_command("KanagawaCompile", function()
-	require("kangawa")
-	vim.api.nvim_exec_autocmds("ColorScheme", { modeline = false })
-end, {})
-
-return M
--- lua/kangawa/init.lua
+++ lua/kangawa/init.lua
@@ -1,13 +1,19 @@
-if vim.g.colors_name then
-	vim.cmd("hi clear")
-end
+local M = {}
 
-vim.g.colors_name = "kanagawa"
-vim.o.termguicolors = true
+M.setup = function(config)
+	if vim.g.colors_name then
+		vim.cmd("hi clear")
+	end
 
-for _, highlight in ipairs({ "editor", "plugins", "lsp", "treesitter" }) do
-	local mod = require("kangawa." .. highlight)
-	for hl, spec in pairs(mod) do
-		vim.api.nvim_set_hl(0, hl, spec)
+	vim.g.colors_name = "kanagawa"
+	vim.o.termguicolors = true
+
+	for _, highlight in ipairs({ "editor", "plugins", "lsp", "treesitter" }) do
+		local mod = require("kangawa." .. highlight)
+		for hl, spec in pairs(mod) do
+			vim.api.nvim_set_hl(0, hl, spec)
+		end
 	end
 end
+
+return M

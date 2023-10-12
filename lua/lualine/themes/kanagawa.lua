119bb4988d550bacb7a1588dd89563ba4ae337a2
Chris Nguyen <chris.dungnguyen@gmail.com>
2023-10-12 17:33:54 +0700

    feat: update lualine theme

--- lua/lualine/themes/kanagawa.lua
+++ lua/lualine/themes/kanagawa.lua
@@ -3,32 +3,32 @@ local theme = require("kanagawa.theme")
 local kanagawa = {}
 
 kanagawa.normal = {
-	a = { bg = theme.syn.fun, fg = theme.ui.bg_m3 },
+	a = { bg = theme.ui.bg, fg = theme.syn.fun, gui = "bold" },
 	b = { bg = theme.diff.change, fg = theme.syn.fun },
 	c = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
 	z = { bg = theme.ui.bg, fg = theme.syn.fun }
 }
 
 kanagawa.insert = {
-	a = { bg = theme.diag.ok, fg = theme.ui.bg },
+	a = { bg = theme.ui.bg, fg = theme.diag.ok },
 	b = { bg = theme.ui.bg, fg = theme.diag.ok },
 	z = { bg = theme.ui.bg, fg = theme.diag.ok }
 }
 
 kanagawa.command = {
-	a = { bg = theme.syn.operator, fg = theme.ui.bg },
+	a = { bg = theme.ui.bg, fg = theme.syn.operator },
 	b = { bg = theme.ui.bg, fg = theme.syn.operator },
 	z = { bg = theme.ui.bg, fg = theme.syn.operator }
 }
 
 kanagawa.visual = {
-	a = { bg = theme.syn.keyword, fg = theme.ui.bg },
+	a = { bg = theme.ui.bg, fg = theme.syn.keyword },
 	b = { bg = theme.ui.bg, fg = theme.syn.keyword },
 	z = { bg = theme.ui.bg, fg = theme.syn.keyword }
 }
 
 kanagawa.replace = {
-	a = { bg = theme.syn.constant, fg = theme.ui.bg },
+	a = { bg = theme.ui.bg, fg = theme.syn.constant },
 	b = { bg = theme.ui.bg, fg = theme.syn.constant },
 }
 

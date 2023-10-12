785c0ceee26824c4fd6d0d11dff82bf06279bb0b
Chris Nguyen <chris.dungnguyen@gmail.com>
2023-10-12 17:10:25 +0700

    feat: update lualine theme

--- lua/lualine/themes/kanagawa.lua
+++ lua/lualine/themes/kanagawa.lua
@@ -3,25 +3,28 @@ local theme = require("kanagawa.theme")
 local kanagawa = {}
 
 kanagawa.normal = {
-	-- a = { bg = theme.syn.fun, fg = theme.ui.bg_m3 },
-	a = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
+	a = { bg = theme.syn.fun, fg = theme.ui.bg_m3 },
 	b = { bg = theme.diff.change, fg = theme.syn.fun },
 	c = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
+	z = { bg = theme.ui.bg, fg = theme.ui.fun }
 }
 
 kanagawa.insert = {
 	a = { bg = theme.diag.ok, fg = theme.ui.bg },
 	b = { bg = theme.ui.bg, fg = theme.diag.ok },
+	z = { bg = theme.ui.bg, fg = theme.ui.ok }
 }
 
 kanagawa.command = {
 	a = { bg = theme.syn.operator, fg = theme.ui.bg },
 	b = { bg = theme.ui.bg, fg = theme.syn.operator },
+	z = { bg = theme.ui.bg, fg = theme.ui.operator }
 }
 
 kanagawa.visual = {
 	a = { bg = theme.syn.keyword, fg = theme.ui.bg },
 	b = { bg = theme.ui.bg, fg = theme.syn.keyword },
+	z = { bg = theme.ui.bg, fg = theme.ui.keyword }
 }
 
 kanagawa.replace = {
